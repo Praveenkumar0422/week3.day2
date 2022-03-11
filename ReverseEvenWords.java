80f4e1ad3a5af7fc6ef33a6d44f45dfa165d8b78
Praveenkumar S <100127748+Praveenkumar0422@users.noreply.github.com>
2022-03-11 13:28:08 +0530

    Add files via upload

---
+++ ReverseEvenWords.java
@@ -0,0 +1,62 @@
+package week3.day2;
+
+import java.util.Arrays;
+
+public class ReverseEvenWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String input = "I am software engineer";
+		
+		String outputString = "";
+		
+		String[] splitWords = input.split(" ");
+		
+		System.out.println(Arrays.toString(splitWords));
+		
+		int splitArrayLength = splitWords.length;
+		System.out.println("Array length:" + splitArrayLength);
+		
+		for (int i = 0; i < splitArrayLength; i++) {
+			
+			if ((i + 1) % 2 == 0) {
+		
+				char[] charArray = splitWords[i].toCharArray();
+				
+				String reverseWord = "";
+				
+				for (int j = charArray.length - 1; j >= 0; j--) {
+					
+					reverseWord = reverseWord + charArray[j];
+					
+				}
+				outputString = outputString+reverseWord+" ";
+				
+			} else {
+				outputString = outputString+splitWords[i]+" ";
+			}
+		}
+		
+		System.out.println(outputString);
+	}}
+		
+		
+		
+				
+				
+		
+	
+		
+		
+			
+				
+				
+				
+		
+		
+		
+
+	
+
+
